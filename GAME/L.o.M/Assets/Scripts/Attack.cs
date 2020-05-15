eff7a51318a1584e7a3c4656a74527c3453b798b
Kevin Elain <47543964+LordElain@users.noreply.github.com>
2020-05-15 10:26:47 +0200

    Animationen
    - Idle & Attack Animation
    - Einbindung

---
+++ GAME/L.o.M/Assets/Scripts/Attack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    private Animator animatorComp = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animatorComp = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            animatorComp.SetTrigger("Hit");
+        }
+    }
+}
