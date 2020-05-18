9ad501d0fdc03fedeb444d421faed92a423288a9
Kevin Elain <47543964+LordElain@users.noreply.github.com>
2020-05-18 23:37:55 +0200

    Lauf Animation
    - Lauf Animation erstellt
    - Lauf Script erstellt

---
+++ GAME/L.o.M/Assets/Scripts/Run_Animation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Run_Animation : MonoBehaviour
+{
+    private Animator animatorComp = null;
+    public GameObject Spieler;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animatorComp = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            CharacterControl Play = Spieler.GetComponent<CharacterControl>();
+            if(Play.speed > 0)
+            {
+                animatorComp.SetTrigger("Run");
+            }
+            
+        }
+    }
+}
