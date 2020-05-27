3f00e93ce6bcc4e587bd263644d8cc9e3c15eb10
Kevin Elain <47543964+LordElain@users.noreply.github.com>
2020-05-27 11:29:47 +0200

    Intro fixed
    Intro wechselt automatisch die Szene, wenn das Video vorbei ist

--- GAME/L.o.M/Assets/Scripts/Intro/SkipOnClick.cs
+++ GAME/L.o.M/Assets/Scripts/Intro/SkipOnClick.cs
@@ -5,16 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class SkipOnClick : MonoBehaviour
 {
+    public UnityEngine.Video.VideoClip videoClip;
     // Start is called before the first frame update
     void Start()
     {
-        
+        var videoPlayer = gameObject.AddComponent<UnityEngine.Video.VideoPlayer>();
+        videoPlayer.playOnAwake = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        var vp = GetComponent<UnityEngine.Video.VideoPlayer>();
+       
         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("MainLevel");
+        }
+
+        if (vp.isPlaying == false)
         {
             SceneManager.LoadScene("MainLevel");
         }
