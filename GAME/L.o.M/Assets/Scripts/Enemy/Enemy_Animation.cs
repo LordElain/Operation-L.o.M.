5f02b1493beb5cc80fd28a05e7cfef47cdc8e9ce
Kevin Elain <47543964+LordElain@users.noreply.github.com>
2020-05-30 16:35:12 +0200

    Neue Ordnerstruktur
    - Neuer Ordnerstruktur um Verwirrung zu vermeiden
    - Animator & State Machine für Enemy angefangen
    - Health Bar & Health Lost erstellt

---
+++ GAME/L.o.M/Assets/Scripts/Enemy/Enemy_Animation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Animation : MonoBehaviour
+{
+    public EnemyControl Enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemy = GetComponent<EnemyControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
