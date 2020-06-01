042c7020806a49a8140d01d151e5502bc405fe45
Kevin Elain <47543964+LordElain@users.noreply.github.com>
2020-06-01 13:20:10 +0200

    State Machine fixed
    - Animation Fixed
    - Root Motion aktiviert

--- GAME/L.o.M/Assets/Scripts/Enemy/Enemy_Animation.cs
+++ GAME/L.o.M/Assets/Scripts/Enemy/Enemy_Animation.cs
@@ -6,40 +6,39 @@ public class Enemy_Animation : MonoBehaviour
 {
     private Animator animatorComp = null;
     public GameObject Following;
-    public GameObject Attack;
+    public GameObject Attacking;
     // Start is called before the first frame update
     void Start()
     {
         animatorComp = GetComponent<Animator>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
         Follow FollowScript = Following.GetComponent<Follow>();
-        EnemyControl AttackScript = Attack.GetComponent<EnemyControl>();
+        EnemyControl AttackScript = Attacking.GetComponent<EnemyControl>();
         Debug.Log(FollowScript.detectPlayer);
         if (FollowScript.detectPlayer == true)
         {
             animatorComp.SetTrigger("Run");
-            animatorComp.SetBool("Idle_Status", false);
-            animatorComp.SetBool("Run_Status", true);
+            animatorComp.SetBool("IdleStatus", false);
+            animatorComp.SetBool("RunStatus", true);
         }
        else
         {
-            animatorComp.SetBool("Idle_Status", true);
-            animatorComp.SetBool("Run_Status", false);
+            animatorComp.SetBool("IdleStatus", true);
+            animatorComp.SetBool("RunStatus", false);
         }
 
         if (AttackScript.attackStatus == true)
         {
             animatorComp.SetTrigger("Attack");
-            animatorComp.SetBool("Idle_Status", false);
+            animatorComp.SetBool("IdleStatus", false);
         }
         else
         {
-            animatorComp.SetBool("Idle_Status", true);
+            animatorComp.SetBool("IdleStatus", true);
         }
     }
 }
