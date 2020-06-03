8860470b20beebd9d08fd305328c20cec6a0fb1c
Kevin Elain <47543964+LordElain@users.noreply.github.com>
2020-06-03 11:50:08 +0200

    HUD and Model Fix
    - Enemy Model Normals umgedreht
    - HUD Lebens Leiste, Leben verloren und von Gegnern gesehen - eingefügt
    - Script angefangen

---
+++ GAME/L.o.M/Assets/Scripts/UI/UI_HealthBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_HealthBar : MonoBehaviour
+{
+    public GameObject HUD;
+    public GameObject Following;
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Canvas HUDScript = HUD.GetComponent<Canvas>();
+        Follow FollowScript = Following.GetComponent<Follow>();
+        CharacterControl PlayerScript = Player.GetComponent<CharacterControl>();
+
+        if (PlayerScript.playerHealth == 5)
+        {
+            HUDScript.Hea
+        }
+
+
+    }
+}
