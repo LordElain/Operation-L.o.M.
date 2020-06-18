4ff3879b7c0e49f56c66ccc22e9be1d540714b71
Kevin Elain <47543964+LordElain@users.noreply.github.com>
2020-06-18 10:05:00 +0200

    Walkie Talkie
    - Audio File spielt wenn man sich in der Nähe befindet

---
+++ GAME/L.o.M/Assets/Scripts/player/WalkieTalkie.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkieTalkie : MonoBehaviour
+{
+    public GameObject Player;
+    public AudioClip Walkie;
+    AudioSource audioData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(Player.transform.position, this.transform.position) < 2)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                audioData.Play();
+                Debug.Log("Spieler ist in Reichweite");
+            }
+        }
+
+    }
+}
